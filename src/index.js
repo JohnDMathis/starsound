34876e60ec787d1c32680da64bca36b15102fce2
JohnDMathis <john.mathis@leankit.com>
2015-02-16 23:16:16 -0600

    add config, move data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
+var config = require( "configya" )(
+	{
+		file: "./config.json",
+		defaults: { dataStreamMap: {}, useArduino: false }
+	} );
+
+var KeyReader;
+if ( config.useArduino ) {
+	require( "./duino.js" );
+}
+if ( config.useConsole ) {
+	KeyReader = require( './keyReader.js' );
+}
+
 var host = require( 'autohost' );
-var KeyReader = require( './keyReader.js' );
 var importer = require( './importer.js' );
 var _ = require( 'lodash' );
 var Transform = require( './transformer.js' );
 var player = require( './player.js' );
 var postal = require( 'postal' );
-// require( "./duino.js" );
+
 var commands = postal.channel( 'commands' );
 
 host.init( {
@@ -20,7 +33,6 @@ host.on( "socket.client.closed", function( data ) {
 
 host.on( "socket.client.identified", function( data ) {
 	console.log( "client identified", data.id );
-	host.socket.notify( "hey.there", { data: 'BOOM' } );
 	player.startNofication( host.socket );
 } );
 
@@ -32,27 +44,15 @@ commands.subscribe( "action", function( actionNum ) {
 commands.subscribe( "analog.changed.*", function( percentage ) {
 	var stretchVals = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
 	var i = Math.round( percentage / stretchVals.length - 1 );
-	// console.log( 'new percentage: %s %; i: %s', percentage, i );
 	setStretch( stretchVals[ i ] );
 } );
 
-var G117 = importer.import( "G117-rel-flux-t1" );
-var comp1 = importer.import( "G117-rel-flux-c2" );
-var comp2 = importer.import( "G117-rel-flux-c3" );
-var comp2b = importer.import( "G117-rel-flux-c3-b" );
-var gd66 = importer.import( "GD66-rel-flux-T" );
-var gd662 = importer.import( "GD66-0206" );
-var comp3 = importer.import( "GD66-rel-flux-C2" );
 
-player.addDataStream( 'G117', G117 );
-player.addDataStream( 'GD66', gd66 );
-player.addDataStream( 'GD662', gd662 );
-player.addDataStream( 'comp1', comp1 );
-player.addDataStream( 'comp2', comp2 );
-player.addDataStream( 'comp2b', comp2b );
-player.addDataStream( 'comp3', comp3 );
-player.addDataStream( 'boring', [ 0.1, 0.2, 0.3, 0.4, 0.5, 0.4, 0.3, 0.2 ] );
-player.addDataStream( 'boringer', [ 0.1, 0.1, 0.15, 0.09, 0.1, 0.11, 0.09, 0.1 ] );
+_.each( config.dataStreamMap, function( item ) {
+	console.log( 'import and add dataStream', item );
+	var stream = importer.import( "./data/" + item );
+	player.addDataStream( item, stream );
+} );
 
 
 player.addTransformer( 'a', function( stream ) {
@@ -75,30 +75,18 @@ player.addTransformer( 'b', function( stream ) {
 
 function playSource( source ) {
 
+	var sourceName = config.dataStreamMap[ source.toString() ];
 
-	var sourceMap = {
-		1: 'comp1',
-		2: 'comp2',
-		3: 'comp3',
-		4: 'G117',
-		5: 'GD66',
-		6: "boring",
-		7: "boringer",
-		8: "GD662"
-	};
-	var sourceName = sourceMap[ source ];
+	console.log( 'selected', sourceName );
+
 	if ( sourceName ) {
-
-		// console.log( 'playSource; [%s] selected; [%s] is currently [%s]',
-		// 	sourceName,
-		// 	player.getStreamName(),
-		// 	player.isPlaying ? "playing" : "stopped" );
 
 		if ( player.getStreamName() === sourceName && player.isPlaying ) {
 			player.stop();
 		} else {
 			player.play( sourceName );
 		}
+
 	} else {
 		console.log( 'not found', source );
 	}
@@ -180,27 +168,28 @@ player.addTransformer( 'custom', function( stream ) {
 		.result();
 } );
 
-var keyActionMap = {
-	1: playSource,
-	2: playSource,
-	3: playSource,
-	4: playSource,
-	5: playSource,
-	6: playSource,
-	7: playSource,
-	8: playSource,
-	9: playSource,
-	a: transform,
-	b: transform,
-	c: transform,
-	d: transform,
-	t: truncate,
-	'backspace': clearTransform,
-	'up': magnify,
-	'down': demagnify,
-	'left': contract,
-	'right': expand,
-	' ': dump
-};
 
-KeyReader( keyActionMap );
+if ( KeyReader ) {
+	KeyReader( {
+		1: playSource,
+		2: playSource,
+		3: playSource,
+		4: playSource,
+		5: playSource,
+		6: playSource,
+		7: playSource,
+		8: playSource,
+		9: playSource,
+		a: transform,
+		b: transform,
+		c: transform,
+		d: transform,
+		t: truncate,
+		'backspace': clearTransform,
+		'up': magnify,
+		'down': demagnify,
+		'left': contract,
+		'right': expand,
+		' ': dump
+	} );
+}
